0b87aa3bf859ac34825dbfd70fbe36617143001c
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2020-12-05 01:37:08 -0300

    first commit.

--- src/server.ts
+++ src/server.ts
@@ -10,4 +10,4 @@ app.get('/', (request, response) =>
 app.listen(3333, () => {
   console.log('⚡ Server started on port 3333!');
 });
-// Configuration for rule "import/no-cycle" is invalid: Value "∞" should be integer.
+// Configuration for rule "import/no-cycle" is invalid: Value "∞" should be integer.a
