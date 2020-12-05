c66509773d73466d81a1af1c39893c2922cfba36
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2020-12-05 17:14:36 -0300

    Ultimas alterações no projeto

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,12 @@ import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) =>
-  response.json({ message: 'Hello GoStack: ' }),
-);
+app.use(express.json());
+app.use(routes);
+
+// app.get('/', (request, response) =>
+//   response.json({ message: 'Hello GoStack: ' }),
+// );
 
 app.listen(3333, () => {
   console.log('⚡ Server started on port 3333!');
